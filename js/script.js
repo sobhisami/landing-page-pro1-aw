1044ce2a24421043108b0a8708088b0f2bc3bf95
sobhisami <sobhisami073@gmail.com>
2025-06-09 15:44:14 +0300

    make go to top and bar

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/* make toggle btn */
+let btn=document.getElementById("btn")
+let menu=document.getElementById("menu")
+btn.onclick=()=>{
+  menu.classList.toggle('open')
+}
+   window.onload = () => {
+            let top = document.querySelector('.top')
+        
+            window.onscroll = (e) => {
+
+                var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+                if(scrollTop > 300) {
+                    top.classList.add('open')
+                }else {
+                    top.classList.remove('open')
+                }
+            }
+            
+            top.onclick = () => {
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                    })
+            }
+        
+        }
+        
