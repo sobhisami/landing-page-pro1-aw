183daf26646f6ccd0e6decb6a6c1384645a8070d
sobhisami <sobhisami073@gmail.com>
2025-06-19 16:14:13 +0300

    add animation and transtions

--- js/script.js
+++ js/script.js
@@ -26,4 +26,27 @@ btn.onclick=()=>{
             }
         
         }
-        
+        // Function to check if an element is in the viewport
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+function animateElements() {
+  var elements = document.querySelectorAll('.animate-text');
+  elements.forEach(function (el) {
+    if (isElementInViewport(el)) {
+      el.style.opacity = 1;
+      el.style.transform = 'translateY(0)';
+    }
+  });
+}
+
+document.addEventListener('scroll', animateElements);
+
+animateElements();
